3d3d609dbff5b9959906917c82407dbe6a477bde
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-29 10:33:56 +0530

    Create User Login  And User Register Function

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// User Register
+export const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const userExists = await User.findOne({ username });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const password_hash = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ username, password_hash });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Server error during registration", error });
+  }
+};
+
+// User login
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error during login", error });
+  }
+};
