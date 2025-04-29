9d266068b2dac43431694aff9a1e5affe4e2a7f4
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-29 08:53:15 +0530

    Connected DB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { connectDB } from "./config/db.js";
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 4000;
 
