bce07bdc5c1a9e55ef620b4dcbf97a68559facda
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-29 13:53:33 +0530

    Design Nav Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,12 @@ import {
   Navigate,
 } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import BookingForm from "./Pages/BookingForm";
+import Dashboard from "./Pages/Dashboard";
+import Navbar from "./components/Navbar";
 
 function PrivateRoute({ children }) {
   const { user } = useAuth();
@@ -13,35 +19,32 @@ function PrivateRoute({ children }) {
 
 function App() {
   return (
-    <>
-      {" "}
-      <AuthProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route
-              path="/booking"
-              element={
-                <PrivateRoute>
-                  <BookingForm />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/booking"
+            element={
+              <PrivateRoute>
+                <BookingForm />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
