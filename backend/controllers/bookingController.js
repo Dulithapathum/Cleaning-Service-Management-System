51fb9e5910a6c14fd3dbf109892489d6e0d738fb
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-29 10:09:59 +0530

    Create Add Booking Function

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from "../models/Booking.js";
 
+// Get Booking
 exports.getBookings = async (req, res) => {
   try {
     const bookings = await Booking.find({ user_id: req.user.id }).populate(
@@ -8,5 +9,27 @@ exports.getBookings = async (req, res) => {
     res.json(bookings);
   } catch (error) {
     res.status(500).json({ message: "Error fetching bookings", error });
+  }
+};
+
+// Add Booking
+exports.addBooking = async (req, res) => {
+  const { customer_name, address, date_time, service_id } = req.body;
+  if (!customer_name || !address || !date_time || !service_id) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const newBooking = new Booking({
+      customer_name,
+      address,
+      date_time,
+      service_id,
+      user_id: req.user.id,
+    });
+    await newBooking.save();
+    res.status(201).json(newBooking);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating booking", error });
   }
 };
