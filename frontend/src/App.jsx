f922d9d25189ff2873b16c2165efe7acc1ecd547
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-04-29 13:36:08 +0530

    Create Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,48 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+
+function PrivateRoute({ children }) {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/signin" />;
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      {" "}
+      <AuthProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/booking"
+              element={
+                <PrivateRoute>
+                  <BookingForm />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </>
+  );
 }
 
 export default App;
